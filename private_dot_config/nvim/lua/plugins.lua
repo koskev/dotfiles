ccaf7d0e1ffc1cd91628f3420f6f3a54a797dd42
Kevin <kevin@kokev.de>
2024-07-15 21:32:47 +0200

    Update vim config

--- private_dot_config/nvim/lua/plugins.lua
+++ private_dot_config/nvim/lua/plugins.lua
@@ -18,6 +18,7 @@ return require('packer').startup(function(use)
 		'nvim-telescope/telescope.nvim', branch = '0.1.x',
 		requires = { {'nvim-lua/plenary.nvim'} }
 	}
+	use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
 
 	use({
 		'rose-pine/neovim',
