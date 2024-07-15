6f58d65ef6d2fcf7a3bf901f7722d5f1bbd22c44
Kevin <kevin@kokev.de>
2024-07-15 21:32:44 +0200

    Add missing configs

---
+++ private_dot_config/zsh/plugins.zsh
@@ -0,0 +1,20 @@
+[[ -e ${ZDOTDIR:-~}/.antidote ]] ||
+  git clone https://github.com/mattmc3/antidote.git ${ZDOTDIR:-~}/.antidote
+
+# Source antidote.
+source ${ZDOTDIR:-~}/.antidote/antidote.zsh
+
+# Initialize antidote's dynamic mode, which changes `antidote bundle`
+# from static mode.
+source <(antidote init)
+# Load the oh-my-zsh's library.
+antidote bundle ohmyzsh/ohmyzsh path:lib
+antidote bundle ohmyzsh/ohmyzsh path:plugins/git
+antidote bundle ohmyzsh/ohmyzsh path:plugins/svn
+
+# Syntax highlighting bundle.
+antidote bundle zsh-users/zsh-syntax-highlighting
+
+antidote bundle zsh-users/zsh-autosuggestions
+
+antidote bundle qwelyt/endless-dog
