b541b9887a580be96dc3799d27ba4842af57b00f
Kevin <kevin@kokev.de>
2024-11-16 20:24:54 +0100

    [nvim] Fix line movement

--- private_dot_config/nvim/lua/set.lua
+++ private_dot_config/nvim/lua/set.lua
@@ -20,5 +20,5 @@ vim.opt.mouse = ""
 
 vim.cmd("au BufWrite *.go :silent !goimports -w %")
 
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv'")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv'")
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
