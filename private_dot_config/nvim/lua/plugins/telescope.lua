37e82a1a38cf89b53c0c6fe6378eed7674b52be6
Kevin <kevin@kokev.de>
2024-11-17 10:09:09 +0100

    [nvim] LSP and spelling telescope keybinds

--- private_dot_config/nvim/lua/plugins/telescope.lua
+++ private_dot_config/nvim/lua/plugins/telescope.lua
@@ -1,7 +1,8 @@
 return {
-	'nvim-telescope/telescope.nvim', branch = '0.1.x',
-	dependencies = { {'nvim-lua/plenary.nvim'} },
-	config = function ()
+	'nvim-telescope/telescope.nvim',
+	branch = '0.1.x',
+	dependencies = { { 'nvim-lua/plenary.nvim' } },
+	config = function()
 		local builtin = require('telescope.builtin')
 		vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = "Find files" })
 		vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = "Live grep" })
@@ -9,7 +10,12 @@ return {
 		vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = "Help" })
 		vim.keymap.set('n', '<leader>fc', builtin.commands, { desc = "Commands" })
 
-		vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle, { desc = "Undo tree"})
+		vim.keymap.set('n', '<leader>lr', builtin.lsp_references, { desc = "LSP references" })
+		vim.keymap.set('n', '<leader>li', builtin.lsp_references, { desc = "LSP implementations" })
+
+		vim.keymap.set('n', '<leader>ss', builtin.spell_suggest, { dec = "Spelling suggestions" })
+
+		vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle, { desc = "Undo tree" })
 	end
 
 }
