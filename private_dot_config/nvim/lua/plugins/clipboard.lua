d1f8443c32a0642545c9281adad9f0d9f75815a7
Kevin <kevin@kokev.de>
2024-11-17 13:15:12 +0100

    [nvim] add neoclip

---
+++ private_dot_config/nvim/lua/plugins/clipboard.lua
@@ -0,0 +1,15 @@
+return {
+	"AckslD/nvim-neoclip.lua",
+	dependencies = {
+		{ 'kkharji/sqlite.lua',           module = 'sqlite' },
+		{ 'nvim-telescope/telescope.nvim' },
+	},
+	config = function()
+		require('neoclip').setup({
+			enable_persistent_history = true,
+			history = 100000,
+		})
+		require('telescope').load_extension('neoclip')
+		vim.keymap.set('n', '<leader>fn', ":Telescope neoclip<CR>", { desc = "Find neoclip" })
+	end,
+}
