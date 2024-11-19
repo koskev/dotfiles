3a0001c32d4ac001ebdf2c003a1023e4738fa946
Kevin <kevin@kokev.de>
2024-11-19 21:29:04 +0100

    [nvim] Add lazygit

--- private_dot_config/nvim/lua/plugins/git.lua
+++ private_dot_config/nvim/lua/plugins/git.lua
@@ -18,5 +18,25 @@ return {
       date_format = "%m-%d-%Y %H:%M:%S", -- template for the date, check Date format section for more options
       virtual_text_column = 1, -- virtual text start column, check Start virtual text at column section for more options
     },
+  },
+  {
+    "kdheepak/lazygit.nvim",
+    lazy = true,
+    cmd = {
+      "LazyGit",
+      "LazyGitConfig",
+      "LazyGitCurrentFile",
+      "LazyGitFilter",
+      "LazyGitFilterCurrentFile",
+    },
+    -- optional for floating window border decoration
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    -- setting the keybinding for LazyGit with 'keys' is recommended in
+    -- order to load the plugin when the command is run for the first time
+    keys = {
+      { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
+    }
   }
 }
