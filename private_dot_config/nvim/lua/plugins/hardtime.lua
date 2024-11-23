5f4c26f4a937570e409b943aae2817a60e2f9db4
Kevin <kevin@kokev.de>
2024-11-23 17:21:00 +0100

    [nvim] Enable scrollwheel using hardtime. Add vim-notify

--- private_dot_config/nvim/lua/plugins/hardtime.lua
+++ private_dot_config/nvim/lua/plugins/hardtime.lua
@@ -2,6 +2,12 @@ return {
 	"m4xshen/hardtime.nvim",
 	dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
 	opts = {
-		restriction_mode = "hint"
+		restriction_mode = "hint",
+		disabled_keys = {
+			["<Up>"] = {},
+			["<Down>"] = {},
+			["<Left>"] = {},
+			["<Right>"] = {},
+		}
 	}
 }
---
+++ private_dot_config/nvim/lua/plugins/notify.lua
@@ -0,0 +1,9 @@
+return {
+	'rcarriga/nvim-notify',
+	config = function()
+		local notify = require("notify")
+		notify.setup()
+		vim.notify = notify
+	end
+
+}
