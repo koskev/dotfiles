a4084347410d67599368a61ca509aaca2ecc78c4
Kevin <kevin@kokev.de>
2025-03-16 15:41:01 +0100

    [nvim] Add grep string key

--- private_dot_config/nvim/lua/plugins/telescope.lua
+++ private_dot_config/nvim/lua/plugins/telescope.lua
@@ -15,6 +15,7 @@ return {
 
 		local builtin = require('telescope.builtin')
 		vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = "Find files" })
+		vim.keymap.set('n', '<leader>fF', builtin.grep_string, { desc = "Grep string" })
 		vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = "Live grep" })
 		vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = "Buffers" })
 		vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = "Help" })
