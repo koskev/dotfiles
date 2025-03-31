5a976c975722874a78c3d459dea18158f956b729
Kevin <kevin@kokev.de>
2025-03-31 22:23:03 +0200

    [nvim] Don't run goimports on save (now a separate function)

--- private_dot_config/nvim/lua/set.lua
+++ private_dot_config/nvim/lua/set.lua
@@ -18,7 +18,7 @@ vim.opt.mouse = ""
 
 -- vim.cmd("set notimeout")
 
-vim.cmd("au BufWrite *.go :silent !goimports -w %")
+-- vim.cmd("au BufWrite *.go :silent !goimports -w %")
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
