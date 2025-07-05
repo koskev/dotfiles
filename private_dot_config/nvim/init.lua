27699e2d9476f5551a61fcfa81ced7bf26d4f766
Kevin <kevin@kokev.de>
2025-07-05 19:43:32 +0200

    [nvim] enable all lsps on startup

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -1,7 +1,20 @@
+local function enable_all_lsps()
+	local lsp_configs = {}
+
+	for _, f in pairs(vim.api.nvim_get_runtime_file('lsp/*.lua', true)) do
+		local server_name = vim.fn.fnamemodify(f, ':t:r')
+		table.insert(lsp_configs, server_name)
+	end
+
+	vim.lsp.enable(lsp_configs)
+end
+
 vim.g.mapleader = " "
+
 
 
 require('config.lazy')
 require("remaps")
 require("set")
 
+enable_all_lsps()
