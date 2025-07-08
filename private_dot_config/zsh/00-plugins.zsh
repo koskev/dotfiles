75ab4a6c9dc9781c15f670cfecff0a50068c7e35
Kevin <kevin@kokev.de>
2025-07-08 21:27:26 +0200

    [zsh] Use deferred loading for some plugins

--- private_dot_config/zsh/00-plugins.zsh
+++ private_dot_config/zsh/00-plugins.zsh
@@ -7,19 +7,21 @@ source ${ZDOTDIR:-~}/.antidote/antidote.zsh
 # Initialize antidote's dynamic mode, which changes `antidote bundle`
 # from static mode.
 source <(antidote init)
+# Deferred loading
+antidote bundle romkatv/zsh-defer
 # Load the oh-my-zsh's library.
 antidote bundle ohmyzsh/ohmyzsh path:lib
 antidote bundle ohmyzsh/ohmyzsh path:plugins/git
 antidote bundle ohmyzsh/ohmyzsh path:plugins/svn
 
-# Syntax highlighting bundle.
-antidote bundle zsh-users/zsh-syntax-highlighting
-
-antidote bundle zsh-users/zsh-autosuggestions
 
 antidote bundle qwelyt/endless-dog
-antidote bundle junegunn/fzf path:shell
-
-antidote bundle zsh-users/zsh-completions
 
 antidote bundle superbrothers/zsh-kubectl-prompt
+
+zsh-defer antidote bundle zsh-users/zsh-autosuggestions
+zsh-defer antidote bundle junegunn/fzf path:shell
+zsh-defer antidote bundle zsh-users/zsh-completions
+
+# Syntax highlighting bundle. Should be the last plugin
+zsh-defer antidote bundle zsh-users/zsh-syntax-highlighting
