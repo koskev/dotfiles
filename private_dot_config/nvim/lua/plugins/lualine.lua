cb5eb33af1f1f9a74821c8495932f9f3376082e9
Kevin <kevin@kokev.de>
2025-07-23 23:00:20 +0200

    [nvim] Use lualine for lsp progress

--- private_dot_config/nvim/lua/plugins/lualine.lua
+++ private_dot_config/nvim/lua/plugins/lualine.lua
@@ -1,15 +1,39 @@
 -- Cool statusline at the bottom
 return {
-	'nvim-lualine/lualine.nvim',
-	opts = {
-		sections = {
-			lualine_c = {
-				{
-					'filename',
-					path = 1,
+	{
+		'linrongbin16/lsp-progress.nvim',
+		opts = {
+			format = function(client_messages)
+				if #client_messages > 0 then
+					return table.concat(client_messages, " ")
+				end
+				return ""
+			end
+		},
+	},
+	{
+		'nvim-lualine/lualine.nvim',
+		config = function()
+			local lualine = require("lualine")
+			lualine.setup({
+				sections = {
+					lualine_c = {
+						{
+							'filename',
+							path = 1,
+						},
+						function()
+							-- invoke `progress` here.
+							return require('lsp-progress').progress()
+						end,
+					},
 				}
-
-			}
-		}
+			})
+			vim.api.nvim_create_autocmd("LspProgress", {
+				callback = function()
+					lualine.refresh({})
+				end
+			})
+		end
 	}
 }
