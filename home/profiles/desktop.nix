2e2c1765b83599268701ee3c2aa5d815a652d9f0
Kevin <kevin@kokev.de>
2025-08-17 19:48:52 +0200

    [nix] Move alacritty

--- home/profiles/desktop.nix
+++ home/profiles/desktop.nix
@@ -12,6 +12,51 @@
     name = "Adwaita";
     size = 0;
   };
+  programs.alacritty = {
+    enable = true;
+    settings = {
+      colors = {
+        bright = {
+          black = "0x555753";
+          blue = "0x729fcf";
+          cyan = "0x34e2e2";
+          green = "0x8ae234";
+          magenta = "0xad7fa8";
+          red = "0xef2929";
+          white = "0xeeeeec";
+          yellow = "0xfce94f";
+        };
+        normal = {
+          black = "0x2e3436";
+          blue = "0x3465a4";
+          cyan = "0x06989a";
+          green = "0x4e9a06";
+          magenta = "0x75507b";
+          red = "0xcc0000";
+          white = "0xd3d7cf";
+          yellow = "0xc4a000";
+        };
+        primary = {
+          background = "0x000000";
+          foreground = "0xd3d7cf";
+        };
+      };
+      env = {
+        TERM = "xterm-256color";
+      };
+      font = {
+        size = 13;
+      };
+      keyboard.bindings = [
+        {
+          action = "SpawnNewInstance";
+          key = "N";
+          mods = "Control|Shift";
+        }
+      ];
+      scrolling.history = 100000;
+    };
+  };
   imports = [
     ../packages/browser.nix
     inputs.zen-browser.homeModules.twilight
