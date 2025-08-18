3239743681e7bfee1bc718271e5af909ab10b850
Kevin <kevin@kokev.de>
2025-08-18 17:54:31 +0200

    [zsh] Speedup antidote by using a plugins txt

--- chezmoi/private_dot_config/zsh/00-plugins.zsh
+++ chezmoi/private_dot_config/zsh/00-plugins.zsh
@@ -4,23 +4,4 @@
 # Source antidote.
 source ${ZDOTDIR:-~}/.antidote/antidote.zsh
 
-# Initialize antidote's dynamic mode, which changes `antidote bundle`
-# from static mode.
-source <(antidote init)
-# Deferred loading
-antidote bundle romkatv/zsh-defer
-# Load the oh-my-zsh's library.
-antidote bundle ohmyzsh/ohmyzsh path:lib
-antidote bundle ohmyzsh/ohmyzsh path:plugins/git
-antidote bundle ohmyzsh/ohmyzsh path:plugins/svn
-
-antidote bundle qwelyt/endless-dog
-
-zsh-defer antidote bundle superbrothers/zsh-kubectl-prompt
-
-zsh-defer antidote bundle zsh-users/zsh-autosuggestions
-zsh-defer antidote bundle junegunn/fzf path:shell
-zsh-defer antidote bundle zsh-users/zsh-completions
-
-# Syntax highlighting bundle. Should be the last plugin
-zsh-defer antidote bundle zsh-users/zsh-syntax-highlighting
+antidote load ${ZDOTDIR:-$HOME}/plugins/plugins.txt
