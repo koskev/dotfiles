2088b9f99ef8efccbcf0470e22b03fd21d8dade7
Kevin <kevin@kokev.de>
2025-08-19 23:58:19 +0200

    [nix] Cleanup X11 stuff

--- nixos/hosts/kevin-nix/configuration.nix
+++ nixos/hosts/kevin-nix/configuration.nix
@@ -70,22 +70,8 @@
     keyMap = "de";
   };
 
-  # Enable the X11 windowing system.
-  # services.xserver.enable = true;
-
-  # Configure keymap in X11
-  # services.xserver.xkb.layout = "us";
-  # services.xserver.xkb.options = "eurosign:e,caps:escape";
-
   # Enable CUPS to print documents.
   # services.printing.enable = true;
-
-  # Enable sound.
-  # services.pulseaudio.enable = true;
-  # OR
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.libinput.enable = true;
 
   users.groups.plugdev = { };
   # Define a user account. Don't forget to set a password with ‘passwd’.
@@ -113,15 +99,6 @@
     VDPAU_DRIVER = "radeonsi";
     LIBVA_DRIVER_NAME = "radeonsi";
   };
-
-  # programs.firefox.enable = true;
-
-  # List packages installed in system profile.
-  # You can use https://search.nixos.org/ to find more packages (and options).
-  # environment.systemPackages = with pkgs; [
-  #   vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #   wget
-  # ];
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
