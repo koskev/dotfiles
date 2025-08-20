336ba8d748ccb23ef7ef69d7687d99890e337221
Kevin <kevin@kokev.de>
2025-08-20 22:06:35 +0200

    [k9s] Add settings

--- home/packages/kubernetes.nix
+++ home/packages/kubernetes.nix
@@ -2,6 +2,34 @@
 {
   home.packages = with pkgs; [
     kubectl
-    k9s
   ];
+
+  programs.k9s = {
+    enable = true;
+    aliases = {
+      dep = "apps/v1/deployments";
+    };
+    hotKeys = {
+      shift-1 = {
+        shortCut = "Shift-1";
+        description = "Viewing pods";
+        command = "pods";
+      };
+      shift-2 = {
+        shortCut = "Shift-2";
+        description = "Deployments";
+        command = "deployment";
+      };
+      shift-3 = {
+        shortCut = "Shift-3";
+        description = "Configmap";
+        command = "configmap";
+      };
+      shift-4 = {
+        shortCut = "Shift-4";
+        description = "Secret";
+        command = "secret";
+      };
+    };
+  };
 }
