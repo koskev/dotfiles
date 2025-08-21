ef718ac29aac08b67029fc56f927e03ba8f4c520
Kevin <kevin@kokev.de>
2025-08-21 17:26:42 +0200

    Revert "[nix] Add parameter to bootstrap"
    This reverts commit 4da207da768f0bae1cb03674d0c4524099a595f4.

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,4 +2,4 @@
 
 SCRIPT_DIR=$(dirname -- "$( readlink -f -- "$0"; )")
 
-nix --extra-experimental-features "nix-command flakes" run nixpkgs#home-manager -- --extra-experimental-features "nix-command flakes" switch --impure --flake "${SCRIPT_DIR}" "$@"
+nix --extra-experimental-features "nix-command flakes" run nixpkgs#home-manager -- --extra-experimental-features "nix-command flakes" switch --impure --flake "${SCRIPT_DIR}"
