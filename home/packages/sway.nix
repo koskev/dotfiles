f8cc140cd6b99e4734fd5f3df21681d1c725d0cc
Kevin <kevin@kokev.de>
2025-08-21 18:19:25 +0200

    [sway] Only use the package on nixos for now

--- home/packages/sway.nix
+++ home/packages/sway.nix
@@ -1,8 +1,18 @@
-_: {
+{
+  settings,
+  lib,
+  ...
+}:
+{
   wayland.windowManager.sway = {
     enable = true;
     checkConfig = true;
     extraConfig = builtins.readFile ./sway.conf;
     config = null;
+  }
+  // lib.optionalAttrs (!settings.system.nixos) {
+    # Don't use the package on non nix os for now
+    package = null;
   };
+
 }
