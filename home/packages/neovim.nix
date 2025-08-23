0da2713825e19dafe4a48107a5cf34f4a7f559b5
Kevin <kevin@kokev.de>
2025-08-23 10:33:12 +0200

    [nvim] Link lazy lock to flake dir

--- home/packages/neovim.nix
+++ home/packages/neovim.nix
@@ -1,4 +1,16 @@
-{ pkgs, ... }:
+{
+  pkgs,
+  config,
+  settings,
+  ...
+}:
+let
+  linkNvim = name: {
+    "nvim/${name}" = {
+      source = ../../configs/nvim/${name};
+    };
+  };
+in
 {
   programs.neovim = {
     enable = true;
@@ -9,7 +21,13 @@
     export LD_LIBRARY_PATH=${pkgs.sqlite.out}/lib:$LD_LIBRARY_PATH
   '';
 
-  xdg.configFile.nvim = {
-    source = ../../configs/nvim;
-  };
+  # Link lockfile to flake dir to allow for easy updating by lazyvim
+  # Link them one by one to actually link the correct file and not the store
+  xdg.configFile = {
+    "nvim/lazy-lock.json".source =
+      config.lib.file.mkOutOfStoreSymlink "${settings.system.flake}/configs/nvim/lazy-lock.json";
+  }
+  // linkNvim "lua"
+  // linkNvim "after"
+  // linkNvim "init.lua";
 }
