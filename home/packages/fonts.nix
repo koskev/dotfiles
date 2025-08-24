69717c7c6d7c138aefdc5e8b4531b74a774d5496
Kevin <kevin@kokev.de>
2025-08-24 13:10:48 +0200

    [nix] Set explicit fonts

--- home/packages/fonts.nix
+++ home/packages/fonts.nix
@@ -4,7 +4,11 @@
 }:
 {
   home.packages = with pkgs; [
-    nerd-fonts.jetbrains-mono
+    dejavu_fonts # Default font
+    nerd-fonts.dejavu-sans-mono # For nerd-font symbols as fallback
+    gyre-fonts # For waybar. Small and nice font
   ];
-  fonts.fontconfig.enable = true;
+  fonts.fontconfig = {
+    enable = true;
+  };
 }
