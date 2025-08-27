4cd6fcb502a0a3c7b1f68e9ad3fcd9798afd16fa
Kevin <kevin@kokev.de>
2025-08-27 20:21:29 +0200

    [zsh] Add alias for nixGL linking workaround

--- home/packages/zsh.nix
+++ home/packages/zsh.nix
@@ -83,6 +83,9 @@
       sway = "WRL_RENDERER=vulkan WLR_SCENE_DISABLE_DIRECT_SCANOUT=1 WLR_RENDER_NO_EXPLICIT_SYNC=1 sway > /tmp/sway.log 2>&1";
       # Make aliases work with sudo
       sudo = "sudo ";
+      # Override library path to prevent weird linker error with enabled nixGL
+      nh = ''LD_LIBRARY_PATH="" nh'';
+      home-manager = ''LD_LIBRARY_PATH="" home-manager'';
     };
 
     localVariables = {
