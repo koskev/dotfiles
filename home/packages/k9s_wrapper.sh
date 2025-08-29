e7c1620db9bd78e07955af6f0a306fabb0af08f2
Kevin <kevin@kokev.de>
2025-08-29 17:16:41 +0200

    [k9s] Add skin and wrapper for dynamic skins based on environment

---
+++ home/packages/k9s_wrapper.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+KUBERNETES_CONTEXT=$(kubectl config current-context)
+
+SIT_SKIN="${SIT_SKIN:-"rose-pine-moon"}"
+INT_SKIN="${INT_SKIN:-"default"}"
+PROD_SKIN="${PROD_SKIN:-"red"}"
+
+if [[ $KUBERNETES_CONTEXT == *"sit"* ]]; then
+    K9S_SKIN="$SIT_SKIN"
+elif [[ $KUBERNETES_CONTEXT == *"int"* ]]; then
+    K9S_SKIN="$INT_SKIN"
+else
+    # Use prod skin for the rest
+    K9S_SKIN="$PROD_SKIN"
+fi
+    
+K9S_SKIN=$K9S_SKIN k9s "$@"
