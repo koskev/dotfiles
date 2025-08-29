7826d7283115e7778aa0011a39a23822f2c788cc
Kevin <kevin@kokev.de>
2025-08-29 17:17:53 +0200

    [nvim] Add docsonnet snippets

--- configs/nvim/lua/plugins/snippet.lua
+++ configs/nvim/lua/plugins/snippet.lua
@@ -9,6 +9,76 @@ return {
 		dependencies = { "rafamadriz/friendly-snippets" },
 		config = function()
 			require("luasnip.loaders.from_vscode").lazy_load()
+			local ls = require("luasnip")
+			local s = ls.snippet
+			local t = ls.text_node
+			local fmt = require("luasnip.extras.fmt").fmt
+			local rep = require("luasnip.extras").rep
+			local i = ls.insert_node
+			ls.add_snippets("jsonnet", {
+				s(
+					"nv",
+					fmt([[
+					'#{}':: __.val(
+						|||
+							{}
+						|||,
+						__.T.{},
+						self.{},
+						),
+						{}: {},
+					]]
+					, {
+						i(1, "Name"),
+						i(2, "Description"),
+						i(3, "Type"),
+						rep(1),
+						rep(1),
+						i(4, "Default Value"),
+					})
+				),
+				s(
+					"nf",
+					fmt([[
+					'#{}':: __.fn(
+						|||
+							{}
+						|||,
+						[
+
+						]
+					),
+					{}:: {},
+					]]
+					, {
+						i(1, "Name"),
+						i(2, "Description"),
+						rep(1),
+						i(3, "Body"),
+					})
+				),
+				s(
+					"na",
+					fmt("__.arg('{}', __.T.{}, help='{}'),", { i(1, "Name"), i(2, "type"), i(3, "help text") })
+				),
+				s(
+					"no",
+					fmt([[
+					'#{}':: __.obj(
+					    |||
+							{}
+					    |||,
+						),
+						{}: {{{}}},
+					]]
+					, {
+						i(1, "Name"),
+						i(2, "Description"),
+						rep(1),
+						i(3, "Body"),
+					})
+				),
+			})
 		end
 	},
 
