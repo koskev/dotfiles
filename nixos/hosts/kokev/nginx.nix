fe3d4fe8c3950368320920b9c9744ac9e6ce3f89
Kevin <kevin@kokev.de>
2025-09-06 23:28:49 +0200

    [kokev] Fix proxy headers

--- nixos/hosts/kokev/nginx.nix
+++ nixos/hosts/kokev/nginx.nix
@@ -31,6 +31,13 @@
             enableACME = true;
             locations."/" = {
               proxyPass = "http://localhost:${port}";
+              extraConfig = ''
+                proxy_set_header Host $host;
+                proxy_set_header X-Real-IP $remote_addr;
+                proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+                proxy_set_header X-Forwarded-Host $server_name;
+              '';
+
             };
           };
         };
