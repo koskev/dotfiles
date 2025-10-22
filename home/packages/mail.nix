92c9be3cc19530f31271d067edaf0e768c4a619e
Kevin <kevin@kokev.de>
2025-10-22 18:00:31 +0200

    fix(mail): Add patch to make writing calendar entries work

--- home/packages/mail.nix
+++ home/packages/mail.nix
@@ -1,10 +1,19 @@
 { pkgs, ... }:
+let
+  patched-etesync-dav = pkgs.etesync-dav.overrideAttrs (old: {
+    patches = (old.patches or [ ]) ++ [
+      # Patch to fix writing to radicale. Can be removed once https://togithub.com/etesync/etesync-dav/pull/357 is merged
+      ./patches/0001-Fix-usage-with-radicale-3.5.1.patch
+    ];
+  });
+in
 {
   home.packages = with pkgs; [
     thunderbird
   ];
   services.etesync-dav = {
     enable = true;
+    package = patched-etesync-dav;
     serverUrl = "https://etesync.kokev.de";
   };
 }
