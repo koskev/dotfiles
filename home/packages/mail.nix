7d9f20ba06f1e7c4a10720598874e6ced942ae1f
Kevin <kevin@kokev.de>
2025-10-25 10:32:23 +0200

    chore(mail): Get etesync patch from github instead of a local patch

--- home/packages/mail.nix
+++ home/packages/mail.nix
@@ -3,7 +3,11 @@ let
   patched-etesync-dav = pkgs.etesync-dav.overrideAttrs (old: {
     patches = (old.patches or [ ]) ++ [
       # Patch to fix writing to radicale. Can be removed once https://togithub.com/etesync/etesync-dav/pull/357 is merged
-      ./patches/0001-Fix-usage-with-radicale-3.5.1.patch
+      (pkgs.fetchpatch {
+        name = "Fix-usage-with-radicale-3.5.1.patch";
+        url = "https://github.com/etesync/etesync-dav/pull/357.patch";
+        hash = "sha256-hKPakl+v95jb11LfvrQp6+u/ojtD/sLaR2gY/OZeQ+s=";
+      })
     ];
   });
 in
