520fa509d243c3370001ff4598c0b8f45bd3e292
Kevin <kevin@kokev.de>
2025-10-25 10:28:15 +0200

    fix(cantata): Fix compilation issues by manually adding a patch
    ref https://togithub.com/NixOS/nixpkgs/pull/454827

--- home/packages/music.nix
+++ home/packages/music.nix
@@ -3,13 +3,31 @@
   config,
   ...
 }:
+let
+  patched-cantata = pkgs.cantata.overrideAttrs (old: {
+    patches = (old.patches or [ ]) ++ [
+      (pkgs.fetchpatch {
+        name = "fix-build-with-qt-610-qfile-open.patch";
+        url = "https://github.com/nullobsi/cantata/pull/89.patch";
+        hash = "sha256-mxW1OmEVKaPINYns9yQSnS2lWVtVRUPKErYhEWdA7jo=";
+      })
+      (pkgs.fetchpatch {
+        name = "fix-build-with-qt-610-invalidateFilter-deprecated.patch";
+        url = "https://github.com/nullobsi/cantata/pull/90.patch";
+        hash = "sha256-XQCh61Al76daRhw3xja+XhraRenIEWIn3eWGN40ESgw=";
+      })
+
+    ];
+  });
+
+in
 {
 
   home.packages = with pkgs; [
 
     # Music
     mpc
-    cantata
+    patched-cantata
     playerctl
   ];
   # RS version not in repo :/
