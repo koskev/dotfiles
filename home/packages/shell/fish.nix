71eed501aaabbefa9989c7db673ac39412307eed
Kevin <kevin@kokev.de>
2026-01-25 23:42:51 +0100

    feat(fish): Not automatic comma since it breaks pipes
    This reverts commit 115dbdebe1b3bf3e25381f882b8b3b043d17d9e7.

--- home/packages/shell/fish.nix
+++ home/packages/shell/fish.nix
@@ -22,11 +22,6 @@ lib.recursiveUpdate
       fish = {
         enable = true;
         shellAliases = aliases;
-        interactiveShellInit = ''
-          function fish_command_not_found
-            , $argv
-          end
-        '';
       };
     };
   }
