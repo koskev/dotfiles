ae339d18eb1d2a1921966ee3278a1bd444fbc0de
Kevin <kevin@kokev.de>
2026-02-20 19:54:41 +0100

    refactor(nix): Move lsp module

---
+++ modules/homeManager/packages/lsp.nix
@@ -0,0 +1,43 @@
+_: {
+  flake.modules.homeManager.lsp =
+    {
+      pkgs,
+      ...
+    }:
+    {
+      home.packages = with pkgs; [
+        bash-language-server
+        # clangd
+        clang-tools
+        gopls
+        helm-ls
+        marksman
+        openscad-lsp
+        rustup
+        bacon
+        terraform-ls
+        tflint
+        yaml-language-server
+        pyright
+        nodejs
+
+        # nix
+        nil
+        nixd
+
+        # Debugger
+        delve
+
+        # Formatter
+        gofumpt
+        prettier
+        # goimports
+        gotools
+        golangci-lint
+        golangci-lint-langserver
+        golines
+        gomodifytags
+
+      ];
+    };
+}
