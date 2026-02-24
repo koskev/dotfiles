62bd26dc5d4b1851558af18d6b5a4b6b09ece011
Kevin <kevin@kokev.de>
2026-02-24 22:52:57 +0100

    refactor(nix): Move steam deck to flake parts

---
+++ modules/hosts/kevin-deck/default.nix
@@ -0,0 +1,30 @@
+{
+  inputs,
+  ...
+}:
+let
+  hostname = "kevin-deck";
+  username = "kevin";
+in
+{
+  # TODO: There are probably still some packets missing. Check on next update
+  nixConfigs.${hostname} = {
+    system = "x86_64-linux";
+    useHomeManagerModule = true;
+    modules = with inputs.self.modules.nixos; [
+      inputs.jovian.nixosModules.default
+      kevin-deck
+      gaming
+      desktop
+      common
+      wireguard
+    ];
+    users.${username} = {
+      modules = with inputs.self.modules.homeManager; [
+        neovim
+        shell
+        base
+      ];
+    };
+  };
+}
