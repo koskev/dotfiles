33aeb8a6b57a4f6133f7e579688b94554c7b8339
Kevin <kevin@kokev.de>
2026-02-26 23:06:15 +0100

    fix(nix): Add hostname to the hm module as well

--- modules/flake/systems.nix
+++ modules/flake/systems.nix
@@ -52,57 +52,59 @@ in
     type = with lib.types; attrsOf (submodule systemOptions);
   };
   config = {
-    flake = {
-      nixosConfigurations = lib.concatMapAttrs (hostname: hostSettings: {
-        "${hostname}" = inputs.nixpkgs.lib.nixosSystem {
-          modules = [
-            inputs.disko.nixosModules.disko
-            inputs.self.modules.nixos.nixos
-            inputs.self.modules.generic.settings
-            {
-              nixpkgs.hostPlatform = lib.mkDefault hostSettings.system;
-              hostSettings.hostName = lib.mkDefault hostname;
-              _module.args.pkgs-stable = import inputs.nixpkgs-stable {
-                inherit (hostSettings) system;
-              };
-            }
+    flake =
+      let
+        commonHomeManagerModules = [
+          inputs.self.modules.generic.settings
+          inputs.self.modules.homeManager.common
+          {
+            hostSettings.hostName = lib.mkDefault hostname;
+          }
+        ];
 
-          ]
-          ++ hostSettings.modules
-          ++ lib.optional hostSettings.useHomeManagerModule {
-            imports = [
-              inputs.home-manager.nixosModules.home-manager
-              inputs.self.modules.nixos.homeManager
-            ];
-            home-manager.users = lib.concatMapAttrs (username: userSettings: {
-              ${username}.imports = [
-                inputs.self.modules.generic.settings
-                inputs.self.modules.homeManager.common
-              ]
-              ++ userSettings.modules;
-            }) hostSettings.users;
-          };
-        };
-      }) (lib.filterAttrs (n: v: !v.nonNixos) config.nixConfigs);
-      homeConfigurations = lib.concatMapAttrs (
-        hostname: hostSettings:
-        lib.concatMapAttrs (username: userSettings: {
-          "${username}@${hostname}" = inputs.home-manager.lib.homeManagerConfiguration {
-            pkgs = inputs.nixpkgs.legacyPackages.${hostSettings.system};
+      in
+      {
+        nixosConfigurations = lib.concatMapAttrs (hostname: hostSettings: {
+          "${hostname}" = inputs.nixpkgs.lib.nixosSystem {
             modules = [
+              inputs.disko.nixosModules.disko
+              inputs.self.modules.nixos.nixos
               inputs.self.modules.generic.settings
-              inputs.self.modules.homeManager.common
               {
+                nixpkgs.hostPlatform = lib.mkDefault hostSettings.system;
                 hostSettings.hostName = lib.mkDefault hostname;
+                _module.args.pkgs-stable = import inputs.nixpkgs-stable {
+                  inherit (hostSettings) system;
+                };
               }
+
             ]
-            ++ userSettings.modules
-            ++ lib.optional hostSettings.nonNixos {
-              hostSettings.system.nonNixos = true;
+            ++ hostSettings.modules
+            ++ lib.optional hostSettings.useHomeManagerModule {
+              imports = [
+                inputs.home-manager.nixosModules.home-manager
+                inputs.self.modules.nixos.homeManager
+              ];
+              home-manager.users = lib.concatMapAttrs (username: userSettings: {
+                ${username}.imports = commonHomeManagerModules ++ userSettings.modules;
+              }) hostSettings.users;
             };
           };
-        }) hostSettings.users
-      ) config.nixConfigs;
-    };
+        }) (lib.filterAttrs (n: v: !v.nonNixos) config.nixConfigs);
+        homeConfigurations = lib.concatMapAttrs (
+          hostname: hostSettings:
+          lib.concatMapAttrs (username: userSettings: {
+            "${username}@${hostname}" = inputs.home-manager.lib.homeManagerConfiguration {
+              pkgs = inputs.nixpkgs.legacyPackages.${hostSettings.system};
+              modules =
+                commonHomeManagerModules
+                ++ userSettings.modules
+                ++ lib.optional hostSettings.nonNixos {
+                  hostSettings.system.nonNixos = true;
+                };
+            };
+          }) hostSettings.users
+        ) config.nixConfigs;
+      };
   };
 }
