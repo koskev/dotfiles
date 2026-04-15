758667874c99fdfff58ba393b19c3691d0e4b1e4
Kevin <kevin@kokev.de>
2026-04-15 20:04:34 +0200

    feat(hyprland): Support F1-F12 for workspaces

--- modules/homeManager/packages/desktop/windowManager/hyprland.nix
+++ modules/homeManager/packages/desktop/windowManager/hyprland.nix
@@ -145,43 +145,8 @@ _: {
             "$mod SHIFT, Q, killactive,"
             "$mod SHIFT, y, movecurrentworkspacetomonitor, l"
             "$mod SHIFT, x, movecurrentworkspacetomonitor, r"
-            # Switch workspaces with mod + [0-9]
-            "$mod, 1, workspace, 1"
-            "$mod, 2, workspace, 2"
-            "$mod, 3, workspace, 3"
-            "$mod, 4, workspace, 4"
-            "$mod, 5, workspace, 5"
-            "$mod, 6, workspace, 6"
-            "$mod, 7, workspace, 7"
-            "$mod, 8, workspace, 8"
-            "$mod, 9, workspace, 9"
-            "$mod, 0, workspace, 10"
-            "$mod, F1, workspace, 11"
-            "$mod, F2, workspace, 12"
-            "$mod, F3, workspace, 13"
-            "$mod, F4, workspace, 14"
-            "$mod, F5, workspace, 15"
-            "$mod, F6, workspace, 16"
-            "$mod, F7, workspace, 17"
             "$mod, asciicircum, togglespecialworkspace, scratchpad" # asciicircum == ^
             "$mod SHIFT, asciicircum, movetoworkspace, special:scratchpad" # asciicircum == ^
-            "$mod SHIFT, 1, movetoworkspacesilent, 1"
-            "$mod SHIFT, 2, movetoworkspacesilent, 2"
-            "$mod SHIFT, 3, movetoworkspacesilent, 3"
-            "$mod SHIFT, 4, movetoworkspacesilent, 4"
-            "$mod SHIFT, 5, movetoworkspacesilent, 5"
-            "$mod SHIFT, 6, movetoworkspacesilent, 6"
-            "$mod SHIFT, 7, movetoworkspacesilent, 7"
-            "$mod SHIFT, 8, movetoworkspacesilent, 8"
-            "$mod SHIFT, 9, movetoworkspacesilent, 9"
-            "$mod SHIFT, 0, movetoworkspacesilent, 10"
-            "$mod SHIFT, F1, movetoworkspacesilent, 11"
-            "$mod SHIFT, F2, movetoworkspacesilent, 12"
-            "$mod SHIFT, F3, movetoworkspacesilent, 13"
-            "$mod SHIFT, F4, movetoworkspacesilent, 14"
-            "$mod SHIFT, F5, movetoworkspacesilent, 15"
-            "$mod SHIFT, F6, movetoworkspacesilent, 16"
-            "$mod SHIFT, F7, movetoworkspacesilent, 17"
             "$mod, left, movefocus, l"
             "$mod, right, movefocus, r"
             "$mod, up, movefocus, u"
@@ -192,7 +157,17 @@ _: {
             "$mod SHIFT, down, movewindow, d"
             "$mod, e, exec, ${./toggle_group.py} --enable-notify true"
 
-          ];
+            "$mod, 0, workspace, 10"
+            "$mod SHIFT, 0, movetoworkspacesilent, 10"
+          ]
+          ++ builtins.concatMap (val: [
+            "$mod, ${toString val}, workspace, ${toString val}"
+            "$mod SHIFT, ${toString val}, movetoworkspacesilent, ${toString val}"
+          ]) (lib.lists.range 1 9)
+          ++ builtins.concatMap (val: [
+            "$mod, F${toString val}, workspace, ${toString (10 + val)}"
+            "$mod SHIFT, F${toString val}, movetoworkspacesilent, ${toString (10 + val)}"
+          ]) (lib.lists.range 1 12);
         };
       }
       // lib.optionalAttrs config.hostSettings.system.nonNixos {
