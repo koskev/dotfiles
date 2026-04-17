22880081b83aa1e27ce088513943047aca197c59
Kevin <kevin@kokev.de>
2026-04-17 18:54:43 +0200

    fix(go): Add workaround for modernize in gotools

--- modules/homeManager/packages/lsp.nix
+++ modules/homeManager/packages/lsp.nix
@@ -5,39 +5,51 @@ _: {
       ...
     }:
     {
-      home.packages = with pkgs; [
-        bash-language-server
-        # clangd
-        clang-tools
-        gopls
-        helm-ls
-        marksman
-        openscad-lsp
-        rustup
-        bacon
-        tofu-ls
-        tflint
-        yaml-language-server
-        pyright
-        nodejs
+      home.packages =
+        let
+          # Workaround for https://togithub.com/NixOS/nixpkgs/issues/509480
+          gotoolsWithoutModernize = pkgs.symlinkJoin {
+            name = "gotools-without-modernize";
+            paths = [ pkgs.gotools ];
+            postBuild = ''
+              rm -f "$out/bin/modernize"
+            '';
+          };
+        in
+        with pkgs;
+        [
+          bash-language-server
+          # clangd
+          clang-tools
+          gopls
+          helm-ls
+          marksman
+          openscad-lsp
+          rustup
+          bacon
+          tofu-ls
+          tflint
+          yaml-language-server
+          pyright
+          nodejs
 
-        # nix
-        nil
-        nixd
+          # nix
+          nil
+          nixd
 
-        # Debugger
-        delve
+          # Debugger
+          delve
 
-        # Formatter
-        gofumpt
-        prettier
-        # goimports
-        gotools
-        golangci-lint
-        golangci-lint-langserver
-        golines
-        gomodifytags
+          # Formatter
+          gofumpt
+          prettier
+          # goimports
+          gotoolsWithoutModernize
+          golangci-lint
+          golangci-lint-langserver
+          golines
+          gomodifytags
 
-      ];
+        ];
     };
 }
