c623486595c7bd11039dd6e27854066a30aa973a
Kevin <kevin@kokev.de>
2026-04-24 23:36:03 +0200

    feat(nvim): Add json and yaml as default treesitter parsers

--- configs/nvim/lua/plugins/treesitter.lua
+++ configs/nvim/lua/plugins/treesitter.lua
@@ -22,6 +22,8 @@ return {
 					"lua",
 					"go",
 					"jsonnet",
+					"json",
+					"yaml",
 				}, -- list of parsers to install at the start of a neovim session
 				-- border = nil, -- border style for the window (e.g. "rounded", "single"), if nil, use the default border style defined by 'vim.o.winborder'. See :h 'winborder' for more info.
 				-- auto_install = false, -- if enabled, install missing parsers when editing a new file
