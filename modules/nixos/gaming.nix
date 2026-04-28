2a23158aaab5a6e5b1e2d474d21ffe93db36e2c9
Kevin <kevin@kokev.de>
2026-04-28 21:35:48 +0200

    fix(lutris): Add workaround for lutris failing

--- modules/nixos/gaming.nix
+++ modules/nixos/gaming.nix
@@ -10,7 +10,30 @@ _: {
 
       environment.systemPackages = with pkgs; [
         # Gaming
-        lutris
+        # FIXME: Workaround until https://togithub.com/NixOS/nixpkgs/issues/513246 is fixed
+        (pkgs.lutris.override {
+          # Intercept buildFHSEnv to modify target packages
+          buildFHSEnv =
+            args:
+            pkgs.buildFHSEnv (
+              args
+              // {
+                multiPkgs =
+                  envPkgs:
+                  let
+                    # Fetch original package list
+                    originalPkgs = args.multiPkgs envPkgs;
+
+                    # Disable tests for openldap
+                    customLdap = envPkgs.openldap.overrideAttrs (_: {
+                      doCheck = false;
+                    });
+                  in
+                  # Replace broken openldap with the custom one
+                  builtins.filter (p: (p.pname or "") != "openldap") originalPkgs ++ [ customLdap ];
+              }
+            );
+        })
         mangohud
         gamemode
       ];
