541ac8f647b365800579b06171d2a8a5b61195fe
Kevin <kevin@kokev.de>
2026-08-01 11:15:50 +0200

    fix(build): Disable etesync

--- modules/homeManager/packages/desktop/mail.nix
+++ modules/homeManager/packages/desktop/mail.nix
@@ -1,25 +1,12 @@
 _: {
   flake.modules.homeManager.desktop =
     { pkgs, ... }:
-    let
-      patched-etesync-dav = pkgs.etesync-dav.overrideAttrs (old: {
-        patches = (old.patches or [ ]) ++ [
-          # Patch to fix writing to radicale. Can be removed once https://togithub.com/etesync/etesync-dav/pull/357 is merged
-          (pkgs.fetchpatch {
-            name = "Fix-usage-with-radicale-3.5.1.patch";
-            url = "https://github.com/etesync/etesync-dav/pull/357.patch";
-            hash = "sha256-hKPakl+v95jb11LfvrQp6+u/ojtD/sLaR2gY/OZeQ+s=";
-          })
-        ];
-      });
-    in
     {
       home.packages = with pkgs; [
         thunderbird
       ];
       services.etesync-dav = {
-        enable = true;
-        package = patched-etesync-dav;
+        enable = false; # Disabled until the package is working again or I find the motivation to fix it myself
         serverUrl = "https://etesync.kokev.de";
       };
     };
